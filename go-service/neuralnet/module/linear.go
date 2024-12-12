b1570059d9b79f78daee1fca4adfc5f24d9d51ac
Josh Friedson <business@joshfriedson.com>
2024-12-11 21:47:07 -0800

    improve readability and performance of loops

--- go-service/neuralnet/module/linear.go
+++ go-service/neuralnet/module/linear.go
@@ -12,25 +12,25 @@ type linear struct {
 	bias    []float32
 }
 
-func (self *linear) Forward(input any) (any, error) {
+func (self *linear) Forward(inputAny any) (any, error) {
 	// assert input is 1D slice of float32 for the time being
-	inputAssert, ok := input.([]float32)
+	input, ok := inputAny.([]float32)
 	if !ok {
 		return nil, fmt.Errorf("for now, linear input must be []float32")
 	}
 
-	if len(inputAssert) != len(self.weights[0]) {
+	inFeatures := len(self.weights[0])
+	if len(input) != inFeatures {
 		return nil, fmt.Errorf("linear input size is incorrect")
 	}
 
 	// TODO: goroutine this puppy
-	inputLen := len(inputAssert)
-	outputLen := len(self.weights)
-	output := make([]float32, outputLen)
-	for out := 0; out < outputLen; out++ {
+	outFeatures := len(self.weights)
+	output := make([]float32, outFeatures)
+	for out := range outFeatures {
 		var z float32 = 0
-		for in := 0; in < inputLen; in++ {
-			z += self.weights[out][in] * inputAssert[in]
+		for in := 0; in < inFeatures; in++ {
+			z += self.weights[out][in] * input[in]
 		}
 		if self.bias != nil {
 			z += self.bias[out]
@@ -62,13 +62,15 @@ func NewLinear(moduleInfo modelarch.ModuleInfo, modulesParams modelarch.ModulesP
 	if err != nil {
 		panic("linear weights must be a two dimensional array")
 	}
-	if len(weights) < 1 {
-		panic("linear weights has dimension of length 0")
+
+	outFeatures := len(weights)
+	if outFeatures < 1 {
+		panic("linear weights out has length 0")
 	}
-	weightDim0Len := len(weights[0])
-	for _, weightDim0 := range weights {
-		if len(weightDim0) != weightDim0Len {
-			panic("linear weight length must be consistent throughout dimension")
+	inFeatures := len(weights[0])
+	for _, weightsDim1 := range weights {
+		if len(weightsDim1) != inFeatures {
+			panic("linear weights in_features must be consistent")
 		}
 	}
 
@@ -79,9 +81,10 @@ func NewLinear(moduleInfo modelarch.ModuleInfo, modulesParams modelarch.ModulesP
 		if err != nil {
 			panic("linear bias must be a one dimensional array")
 		}
-	}
-	if len(bias) != len(weights) {
-		panic("linear bias size must match weight layers")
+
+		if len(bias) != outFeatures {
+			panic("linear bias size must match weight out_features")
+		}
 	}
 
 	return &linear{weights, bias}
